a010ee30d279d25e560f41a6b50d0cd7474cd01a
matheusab <mafeus.barbosa@gmail.com>
2017-02-01 19:10:34 -0200

    - Fixed the slow execution; instead of adding and removing elements, the new html containing anchors is built incrementally from the original text. - The "match" property is being used to find the matches, instead of "matchExpression"
    TODO add window.resize event listener when calling the highlight method

--- src/highlight.js
+++ src/highlight.js
@@ -8,59 +8,59 @@ function insertAt(string, index, substring) {
 
 Highlight = {
     recalculate: function () {
-        console.log("Recalculando highlights...");
+        console.log("Highlight - recalculate started");
+        var start = new Date();
+
         for (var textSelector in Highlight._highlightContainers) {
             if (Highlight._highlightContainers.hasOwnProperty(textSelector)) {
-                var originalTextEl = $(textSelector);
-                var highlights = Highlight._highlightContainers[textSelector];
-                var highlightContainer = $(originalTextEl).closest('.mab-hl-container');
-                var highlightsContainer = highlightContainer.find(".mab-hl-highlights-container");
-                highlightsContainer.html("");
-
-                var originalTextPaddingTop = parseInt(originalTextEl.css("padding-top"));
-                var originalTextMarginTop = parseInt(originalTextEl.css("margin-top"));
-                var originalTextBorderTop = parseInt(originalTextEl.css("border-top"));
-
-                var originalTextPaddingLeft = parseInt(originalTextEl.css("padding-left"));
-                var originalTextMarginLeft = parseInt(originalTextEl.css("margin-left"));
-                var originalTextBorderLeft = parseInt(originalTextEl.css("border-left"));
-                //todo testar se é NaN
-                var originalTextOffsetTop = originalTextBorderTop + originalTextMarginTop + originalTextPaddingTop;
-                var originalTextOffsetLeft = originalTextBorderLeft + originalTextMarginLeft + originalTextPaddingLeft;
-
-                var newBaseHighlightContainer = originalTextEl.clone();
-                newBaseHighlightContainer.html("");
-                newBaseHighlightContainer
-                    .removeAttr("id")
-                    .removeClass("mab-hl-text-container")
-                    .addClass('mab-hl-highlight-container')
-
-
-                var newAnchorHtml = "<span class='mab-hl-text-anchor'></span>";
-
-                for (var i = 0; i < highlights.length; i++) {
-                    var hl = highlights[i];
-                    var newHtml = insertAt(originalTextEl.html(), hl.index, newAnchorHtml);
-                    originalTextEl.html(newHtml);
-                    var anchorEl = highlightContainer.find(".mab-hl-text-anchor");
-
-                    var span = $("<span tabindex='0'" +
-                        "data-toggle='tooltip' " +
-                        "title='" + hl.replace + "' " +
-                        "class='mab-hl-text-highlight' " +
-                        "style='top: " + (anchorEl[0].offsetTop - originalTextOffsetTop) + "px; margin-left: " + (anchorEl[0].offsetLeft - originalTextOffsetLeft) + "px;'>" +
-                        hl.match +
-                        "</span>");
-                    var el = newBaseHighlightContainer
-                        .clone()
-                        .append(span);
-                    highlightsContainer.append(el);
-                    anchorEl.detach();
-                }
-
-                highlightContainer.find('[data-toggle="tooltip"]').tooltip({html: true});
+                Highlight.appendHighlights(textSelector);
             }
         }
+        var end = new Date();
+        console.log("Highlight - recalculation completed after " + (end - start) / 1000 + " seconds");
+    },
+    appendHighlights: function (textSelector) {
+        var originalTextEl = $(textSelector);
+        var anchors = originalTextEl.find(".mab-hl-text-anchor");
+        var generalContainer = $(originalTextEl).closest('.mab-hl-container');
+        var highlightsContainer = generalContainer.find(".mab-hl-highlights-container");
+        highlightsContainer.html("");
+
+        var originalTextPaddingTop = parseInt(originalTextEl.css("padding-top"));
+        var originalTextMarginTop = parseInt(originalTextEl.css("margin-top"));
+        var originalTextBorderTop = parseInt(originalTextEl.css("border-top"));
+
+        var originalTextPaddingLeft = parseInt(originalTextEl.css("padding-left"));
+        var originalTextMarginLeft = parseInt(originalTextEl.css("margin-left"));
+        var originalTextBorderLeft = parseInt(originalTextEl.css("border-left"));
+        //todo check if not NaN
+        var originalTextOffsetTop = originalTextBorderTop + originalTextMarginTop + originalTextPaddingTop;
+        var originalTextOffsetLeft = originalTextBorderLeft + originalTextMarginLeft + originalTextPaddingLeft;
+
+        var newBaseHighlightContainer = originalTextEl.clone();
+        newBaseHighlightContainer.html("");
+        newBaseHighlightContainer
+            .removeAttr("id")
+            .removeClass("mab-hl-text-container")
+            .addClass('mab-hl-highlight-container');
+        anchors.each(function () {
+
+            var span = $("<span tabindex='0'" +
+                "data-toggle='tooltip' " +
+                "title='" + this.dataset.replace + "' " +
+                "class='mab-hl-text-highlight' " +
+                "style='top: " + (this.offsetTop - originalTextOffsetTop) + "px; margin-left: " + (this.offsetLeft - originalTextOffsetLeft) + "px;'>" +
+                this.dataset.match +
+                "</span>");
+            var el = newBaseHighlightContainer
+                .clone()
+                .append(span);
+            highlightsContainer.append(el);
+
+        });
+
+        generalContainer.find('[data-toggle="tooltip"]').tooltip({html: true});
+
     },
 
     /**
@@ -76,38 +76,59 @@ Highlight = {
         var container = params.textContainerSelector;
         var matches = params.matches;
         var containerEl = $(container);
+        var regExpMatches = [];
+        var texto = containerEl.html();
         containerEl.addClass('mab-hl-text-container');
         containerEl.wrap("<div class='mab-hl-container'></div>");
         containerEl.after("<div class='mab-hl-highlights-container'></div>");
 
+
         console.warn("bug de match nas ÂNCORAS dependendo da expressão");
         console.warn("isso assume que o texto É IMUTÁVEL, visto que as posições são gravadas somente 1x");
-        var regExpMatches = [];
-        var texto = containerEl.html();
-        for (var i = 0; i < matches.length; i++) {
-            var objMatch = matches[i];
-            var pattern = new RegExp(objMatch.matchExpression, 'g');
+        for (var i=0;i<matches.length;i++) {
+            var m = matches[i];
+
+            //var objMatch = matches[i];
+            //var pattern = new RegExp('\\b' + objMatch.match + '\\b', 'g');
+            //before:
+            var pattern = new RegExp(m.match, 'g');
             var match;
-            // acho que o jeito vai ser montar a string aos poucos...
-            // ou melhor, gravo a POSIÇÃO onde devo colocar algo
-            // e vou calculando a partir daí pra montar a string
-            /*var replace = "<span class='mab-hl-text-anchor' " +
-             "data-texto='" + objMatch.match + "' " +
-             "data-tooltip-text='" + objMatch.tooltipText + "'></span>" + objMatch.match;*/
+
             while (match = pattern.exec(texto)) {
                 regExpMatches.push({
                     match: match[0],
                     index: match.index,
-                    replace: objMatch.tooltipText
+                    replace: m.tooltipText
                 });
             }
         }
 
+
+        regExpMatches.sort(function (a, b) {
+            return a.index - b.index;
+        });
+        var newCharCount = 0;
+        var newText = texto;
+        for (var i = 0; i < regExpMatches.length; i++) {
+            var r = regExpMatches[i];
+            var newSpan = "<span class='mab-hl-text-anchor' " +
+                "data-match='" + r.match + "' " +
+                "data-replace='" + r.replace + "'></span>";
+            newText = insertAt(newText, r.index + newCharCount, newSpan);
+            newCharCount += newSpan.length;
+        }
+
+        containerEl.html(newText);
+
+        console.log("Total matches: " + regExpMatches.length);
+        console.log(regExpMatches);
+        // lembrar de ordenar por posição pra facilitar a vida
+
+
         Highlight._highlightContainers[container] = regExpMatches;
 
         Highlight.recalculate();
-    }
-    ,
+    },
     _highlightContainers: {}
 
 
